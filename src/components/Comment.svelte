5b42bf1926c5499ba736cfc7f0afc476a960caf2
Oren Lindsey <o.lindsey@icloud.com>
2022-07-13 17:11:29 -0400

    another bug fix

--- src/components/Comment.svelte
+++ src/components/Comment.svelte
@@ -12,7 +12,7 @@
     <h1 class="font-mono text-blue-900 text-xl"><a href="/users/{comment.poster.name}"><img class="h-10 inline mr-1 rounded-full border-2 border-black" src="https://api.wasteof.money/users/{comment.poster.name}/picture" alt="{comment.poster.name}'s profile picture" />@{comment.poster.name}</a></h1>
     </div>
     <p class="text-center">{@html comment.content}</p>
-    <a href="/comments/{comment.id}" class="text-blue-900 text-sm grid place-items-center">View comment</a>
+    <!--<a href="/comments/{comment.id}" class="text-blue-900 text-sm grid place-items-center">View comment</a>-->
     <a href="https://beta.wasteof.money/posts/{post.id}/comments/{comment.id}" class="text-blue-900 text-sm grid place-items-center">View comment on wasteof</a>
     {#if comment.hasReplies}
     {#if comment.replies.length > 0}
