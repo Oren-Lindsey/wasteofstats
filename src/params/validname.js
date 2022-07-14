7639a3da1aaabf538eed9cb7b1baa5463e81742d
Oren Lindsey <o.lindsey@icloud.com>
2022-07-14 07:28:15 -0400

    bug fixes

--- src/params/validname.js
+++ src/params/validname.js
@@ -1,8 +1,8 @@
 /** @type {import('@sveltejs/kit').ParamMatcher} */
 export function match(param) {
-    const exceptions = ['wasteof.money', 'willy.', 'someonewithalongername', 'wwwwwwwwwwwwwwwwwwwww']
-    if (!exceptions.includes(param)) {
-        return /^[a-z0-9_\\-]{1,20}$/.test(param);
+    const exceptions = ['wasteof.money', 'willy.', 'someonewithalongername', 'wwwwwwwwwwwwwwwwwwwww', 'david (admin)']
+    if (!exceptions.includes(param.toLowerCase())) {
+        return /^[a-z0-9_\\-]{1,20}$/.test(param.toLowerCase());
     } else {
         return true
     }
